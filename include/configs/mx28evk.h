f0b3eff363654bab34093fa0cf34283cfa24125a
Takumi Sueda <puhitaku@gmail.com>
2020-05-15 20:40:53 +0900

    Update default env

--- include/configs/mx28evk.h
+++ include/configs/mx28evk.h
@@ -107,84 +107,6 @@
 
 /* Extra Environment */
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"ubifs_file=filesystem.ubifs\0" \
-	"update_nand_full_filename=u-boot.nand\0" \
-	"update_nand_firmware_filename=u-boot.sb\0"	\
-	"update_nand_firmware_maxsz=0x100000\0"	\
-	"update_nand_stride=0x40\0"	/* MX28 datasheet ch. 12.12 */ \
-	"update_nand_count=0x4\0"	/* MX28 datasheet ch. 12.12 */ \
-	"update_nand_get_fcb_size="	/* Get size of FCB blocks */ \
-		"nand device 0 ; " \
-		"nand info ; " \
-		"setexpr fcb_sz ${update_nand_stride} * ${update_nand_count};" \
-		"setexpr update_nand_fcb ${fcb_sz} * ${nand_writesize}\0" \
-	"update_nand_firmware_full=" /* Update FCB, DBBT and FW */ \
-		"if tftp ${update_nand_full_filename} ; then " \
-		"run update_nand_get_fcb_size ; " \
-		"nand scrub -y 0x0 ${filesize} ; " \
-		"nand write.raw ${loadaddr} 0x0 ${fcb_sz} ; " \
-		"setexpr update_off ${loadaddr} + ${update_nand_fcb} ; " \
-		"setexpr update_sz ${filesize} - ${update_nand_fcb} ; " \
-		"nand write ${update_off} ${update_nand_fcb} ${update_sz} ; " \
-		"fi\0" \
-	"update_nand_firmware="		/* Update only firmware */ \
-		"if tftp ${update_nand_firmware_filename} ; then " \
-		"run update_nand_get_fcb_size ; " \
-		"setexpr fcb_sz ${update_nand_fcb} * 2 ; " /* FCB + DBBT */ \
-		"setexpr fw_sz ${update_nand_firmware_maxsz} * 2 ; " \
-		"setexpr fw_off ${fcb_sz} + ${update_nand_firmware_maxsz};" \
-		"nand erase ${fcb_sz} ${fw_sz} ; " \
-		"nand write ${loadaddr} ${fcb_sz} ${filesize} ; " \
-		"nand write ${loadaddr} ${fw_off} ${filesize} ; " \
-		"fi\0" \
-	"update_nand_kernel="		/* Update kernel */ \
-		"mtdparts default; " \
-		"nand erase.part kernel; " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"${get_cmd} ${image}; " \
-		"nand write ${loadaddr} kernel ${filesize}\0" \
-	"update_nand_fdt="		/* Update fdt */ \
-		"mtdparts default; " \
-		"nand erase.part fdt; " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"${get_cmd} ${fdt_file}; " \
-		"nand write ${loadaddr} fdt ${filesize}\0" \
-	"update_nand_filesystem="		/* Update filesystem */ \
-		"mtdparts default; " \
-		"nand erase.part filesystem; " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"${get_cmd} ${ubifs_file}; " \
-		"ubi part filesystem; " \
-		"ubi create filesystem; " \
-		"ubi write ${loadaddr} filesystem ${filesize}\0" \
-	"nandargs=setenv bootargs console=${console_mainline},${baudrate} " \
-		"rootfstype=ubifs ubi.mtd=6 root=ubi0_0 ${mtdparts}\0" \
-	"nandboot="		/* Boot from NAND */ \
-		"mtdparts default; " \
-		"run nandargs; " \
-		"nand read ${loadaddr} kernel 0x00400000; " \
-		"if test ${boot_fdt} = yes; then " \
-			"nand read ${fdt_addr} fdt 0x00080000; " \
-			"bootz ${loadaddr} - ${fdt_addr}; " \
-		"else " \
-			"if test ${boot_fdt} = no; then " \
-				"bootz; " \
-			"else " \
-				"echo \"ERROR: Set boot_fdt to yes or no.\"; " \
-			"fi; " \
-		"fi\0" \
 	"update_sd_firmware_filename=u-boot.sd\0" \
 	"update_sd_firmware="		/* Update the SD firmware partition */ \
 		"if mmc rescan ; then "	\
@@ -202,9 +124,9 @@
 	"fdt_addr=0x41000000\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
-	"mmcdev=0\0" \
-	"mmcpart=2\0" \
-	"mmcroot=/dev/mmcblk0p3 rw rootwait\0" \
+	"mmcdev=1\0" \
+	"mmcpart=1\0" \
+	"mmcroot=/dev/mmcblk1p2 rw rootwait\0" \
 	"mmcargs=setenv bootargs console=${console_mainline},${baudrate} " \
 		"root=${mmcroot}\0" \
 	"loadbootscript="  \
@@ -228,30 +150,6 @@
 		"else " \
 			"bootz; " \
 		"fi;\0" \
-	"netargs=setenv bootargs console=${console_mainline},${baudrate} " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"netboot=echo Booting from net ...; " \
-		"run netargs; "	\
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"${get_cmd} ${image}; " \
-		"if test ${boot_fdt} = yes; then " \
-			"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
-			"else " \
-				"if test ${boot_fdt} = try; then " \
-					"bootz; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi;" \
-			"fi; " \
-		"else " \
-			"bootz; " \
-		"fi;\0"
 
 #define CONFIG_BOOTCOMMAND \
 	"mmc dev ${mmcdev}; if mmc rescan; then " \
